a3964a798cfbf49f3c20f6140dfb8f00b9ce289b
Daniel Roa <lion35528@gmail.com>
2022-10-25 10:25:20 -0500

    add function capture the option coin

---
+++ game/Game.js
@@ -0,0 +1,10 @@
+const selectOptionCoin = () => {
+
+    console.log(optionCoin.value);
+}
+
+const optionCoin = document.getElementById('optionCoin');
+optionCoin.addEventListener('change', selectOptionCoin);
+// let value = optionCoin.value;
+// console.log(value)
+
