d7bcfa01153b484a88e37df0b00906e1b7dbad24
Daniel Roa <lion35528@gmail.com>
2022-10-25 10:59:48 -0500

    add funtion do bet

--- game/Game.js
+++ game/Game.js
@@ -1,10 +1,21 @@
-const selectOptionCoin = () => {
+import Player from "./Player.js";
 
+const selectOptionCoin = () => {
     console.log(optionCoin.value);
 }
 
 const optionCoin = document.getElementById('optionCoin');
 optionCoin.addEventListener('change', selectOptionCoin);
-// let value = optionCoin.value;
-// console.log(value)
 
+
+const bet = document.getElementById('bet');
+const btnBet = document.getElementById('btnBet');
+
+btnBet.addEventListener('click', () => {
+    // console.log(bet.value);
+    const p = new Player(bet.value);
+    p.setBet(bet.value);
+
+    const betElement = document.getElementById("playerBet");
+    betElement.textContent = "Player: " + p.bet;
+});
