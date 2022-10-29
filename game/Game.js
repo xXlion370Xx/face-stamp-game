33ef2909b512676eb9acb74c1efedc2dfbcdde7e
Daniel Roa <lion35528@gmail.com>
2022-10-29 00:06:09 -0500

    add functions to see the bet, coin clicking play

--- game/Game.js
+++ game/Game.js
@@ -1,22 +1,25 @@
 import Coin from "./Coin.js";
 import Player from "./Player.js";
 
-const selectFaceCoin = () => {
-    const coin = new Coin();
-    coin.setState(optionCoin.value);
+export const selectFaceCoin = () => {
+    const C = new Coin();
+    C.setFace(optionCoin.value);
+
+    return C.face;
 }
 const optionCoin = document.getElementById('optionCoin');
 optionCoin.addEventListener('change', selectFaceCoin);
 
 
 const bet = document.getElementById('bet');
-const btnBet = document.getElementById('btnBet');
 
-const doBet = () => {
+export const doBet = () => {
     const P = new Player();
     P.setBet(bet.value);
 
-    const betElement = document.getElementById("playerBet");
-    betElement.textContent = "Player: " + P.bet;
+    return P;
 }
-btnBet.addEventListener('click', doBet);
+
+export function compareResult(faceCoin, betCoin) {
+    return (faceCoin == betCoin);
+}
--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 import Coin from "./game/Coin.js";
-import Player from "./game/Player.js"
+import { compareResult, doBet, selectFaceCoin } from "./game/Game.js";
 
 
 const play = () => {
     const game = document.getElementById('game');
 
     let faceCoin = Math.floor(Math.random() * 2);
-
     const C = new Coin(faceCoin);
-    C.setState = faceCoin;
+    C.setFace = faceCoin;
 
+    let playerCoin = selectFaceCoin();
+
+    let result = compareResult(C.face, playerCoin);
+
+    const playerBet = doBet();
     game.innerHTML =
-        `<h2>Tu juego:</h2>
-        Cara de la moneda: ${C.state}`;
-
+        `<h1>Tu juego</h1>
+        Cara de la moneda: ${C.face} <br>
+        Tu seleccion: ${playerCoin} <br>
+        Tu apuesta: ${playerBet.bet}
+        `;
 }
 const btnPLay = document.getElementById('btn-play');
 btnPLay.addEventListener("click", play)
