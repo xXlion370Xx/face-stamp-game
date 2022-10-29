be53bf9d046ea7fd923573869e95c679fb8cd88d
Daniel Roa <lion35528@gmail.com>
2022-10-28 23:13:00 -0500

    Revert "merge file game to main"
    This reverts commit f0344e187df86ff00c249edecf5d7e16e218fc5a.

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 import Coin from "./game/Coin.js";
 import Player from "./game/Player.js"
 
-const selectFaceCoin = () => {
-    const coin = new Coin();
-    coin.setState(optionCoin.value);
-}
-const optionCoin = document.getElementById('optionCoin');
-optionCoin.addEventListener('change', selectFaceCoin);
-
-
-const bet = document.getElementById('bet');
-const btnBet = document.getElementById('btnBet');
-
-const doBet = () => {
-    const P = new Player();
-    P.setBet(bet.value);
-
-    const betElement = document.getElementById("playerBet");
-    betElement.textContent = "Player: " + P.bet;
-}
-btnBet.addEventListener('click', doBet);
-
 
 const play = () => {
     const game = document.getElementById('game');
