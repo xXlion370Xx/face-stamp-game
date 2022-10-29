07f0260c8bdc7963b1c76c29e970423a40303b93
Daniel Roa <lion35528@gmail.com>
2022-10-29 12:27:35 -0500

    Add function to show winnner, function to turn number to face

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import Coin from "./game/Coin.js";
 import { compareResult, doBet, selectFaceCoin } from "./game/Game.js";
 
+function turnNumberToFace(num) {
+    num == 0 ? num = "Cara" : num = "Sello";
+    return num;
+}
+
+const showResult = (result) => result == true ? "Ganaste" : "Perdiste";
 
 const play = () => {
     const game = document.getElementById('game');
@@ -11,15 +17,19 @@ const play = () => {
 
     let playerCoin = selectFaceCoin();
 
-    let result = compareResult(C.face, playerCoin);
+    let resultGame = compareResult(C.face, playerCoin);
+    console.log(playerCoin)
 
     const playerBet = doBet();
     game.innerHTML =
         `<h1>Tu juego</h1>
-        Cara de la moneda: ${C.face} <br>
-        Tu seleccion: ${playerCoin} <br>
-        Tu apuesta: ${playerBet.bet}
+        Cara de la moneda: ${turnNumberToFace(C.face)} <br>
+        Tu seleccion: ${turnNumberToFace(playerCoin)} <br>
+        Tu apuesta: ${playerBet.bet} <br>
+        ${showResult(resultGame)}
         `;
+
+
 }
 const btnPLay = document.getElementById('btn-play');
 btnPLay.addEventListener("click", play)
