8901a1c1b54799dad8ffc4d69f095fb82b3d5634
Daniel Roa <lion35528@gmail.com>
2022-10-29 21:26:23 -0500

    update game.js delete funct gamePlayer, (add selectFaceCoin, doBet handleGame), update handletBet and show progress

--- game/Game.js
+++ game/Game.js
@@ -13,32 +13,45 @@ export const randomFaceCoin = () => {
 }
 
 /**
- * Function to save statistics of the player
- * @returns player object
+ * Select the bet DOM element value and set property player.bet
+ * @param {Object} player
+ * @returns Value of bet player object
  */
-export const gamePlayer = () => {
+export const doBet = (player) => {
     const bet = document.getElementById('bet');
-    const optionCoin = document.getElementById('optionCoin');
-
-    const P = new Player();
     let betToInt = parseInt(bet.value);
-    P.setBet(betToInt);
-    P.setCoin(turnNumberToFace(optionCoin.value));
+    player.setBet(betToInt);
 
-    return P;
+    return player.bet;
+}
+
+/**
+ * Select the coin DOM element and set property player.coin
+ * @param {Object} player 
+ * @returns Value of coin face player selected
+ */
+export const selectFaceCoin = (player) => {
+    const coin = document.getElementById('optionCoin');
+    let valueCoin = coin.value;
+    player.setCoin(valueCoin);
+
+    return player.coin;
 }
 
 /**
  * Function to handle the bet depending the result of the game
- * @param {String} resultGame 
- * @param {int} bet 
+ * @param {String} resultGame
+ * @param {Object} player 
  * @returns A int bet param / 2 if the result game equals "Perdiste", else param bet * 2 
  */
-export const handleBet = (resultGame, bet) => {
+export const handleBet = (resultGame, player) => {
     if (resultGame == "Perdiste") {
-        return bet / 2;
+        player.setBet(player.bet / 2);
+        return player.bet;
     }
-    return bet * 2;
+
+    player.setBet(player.bet * 2);
+    return player.bet;
 }
 
 /**
@@ -47,7 +60,7 @@ export const handleBet = (resultGame, bet) => {
  * @param {int} selectedCoin 
  * @returns String "Ganaste" if the params are the same else return "Perdiste" 
  */
-export const showResult = (faceCoin, selectedCoin) => faceCoin == selectedCoin ? "Ganaste" : "Perdiste";
+export const getResult = (faceCoin, selectedCoin) => faceCoin == selectedCoin ? "Ganaste" : "Perdiste";
 
 /**
  * Funtion to convert the property face coin to string
@@ -65,19 +78,25 @@ const turnNumberToFace = (num) => num == 0 ? "Cara" : "Sello";
  * @param {String} resultGame
  */
 export const showProgress = (board, player, coin, resultGame) => {
-    board.innerHTML =
-        `<h1>Tu juego</h1>
-        Cara de la moneda: ${turnNumberToFace(coin.face)} <br>
-        Tu seleccion: ${turnNumberToFace(player.coin)} <br>
-        Tu apuesta: ${player.bet} <br>
-        ${resultGame}
+    let game = document.createElement("div");
+    game.className = "game";
+
+    board.appendChild(game);
+    game.innerHTML =
+        `   
+        <ul>
+            <li>Cara de la moneda: ${turnNumberToFace(coin.face)}</li>
+            <li>Tu seleccion: ${turnNumberToFace(player.coin)}</li>
+            <li>Tu apuesta: ${player.bet}</li>
+            <li>${resultGame}</li>
+        </ul>
     `;
 }
 
 /**
 * Write the statistics player on a ul list
 * @param {Object} Player 
-* @param {String} handleBet
+* @param {String} handleBet 
 */
 export function showStatisticsPlayer(Player, handleBet) {
     const statistics = document.getElementById('playerStatistics');
@@ -89,6 +108,16 @@ export function showStatisticsPlayer(Player, handleBet) {
                <li>Victorias: ${Player.wins}</li>
                <li>Derrotas: ${Player.loses}</li>
                <li>Tu dinero: ${handleBet}</li>
-               <li>Cara del moneda: ${Player.coin}</li>
+               <li>Seleccion lado de la moneda: ${turnNumberToFace(Player.coin)}</li>
            </ul>`
+}
+
+export const handleGameStatistics = (resultGame, player) => {
+    let wins = player.wins;
+
+    if (resultGame == "Ganaste") {
+        player.setWins(wins + 1);
+    } else {
+        player.setLoses(wins - 1);
+    }
 }
